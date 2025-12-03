4f67a09c44ca428f2b947d6e5b88893e27be13db
aabel3 <avraham.lewis.abel@gmail.com>
2025-12-03 10:28:22 -0500

    Add files via upload

---
+++ lab1b.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+personName = "John" 
+
+print("How old are you " + personName + "?")
---
+++ lab1c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+personName = "John" 
+age = 72
+
+print("John is " +str(age) + " years old!")
